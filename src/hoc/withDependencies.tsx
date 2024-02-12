d28eaf41941498bac273177cc9956907db72b2c0
Rahman Mansouri <Rahman.Mansouri@cineplex.com>
2024-02-12 10:05:09 -0500

    Revert "define withSupspense hoc and wrapp all components with it for enabling react.lazy function"
    This reverts commit 83a729eeb077e29f8255797b6be1de0d6c104f9f.

--- src/hoc/withDependencies.tsx
+++ src/hoc/withDependencies.tsx
@@ -1,11 +1,5 @@
-// This is a Higher-Order Component (HOC) used for Dependency Injection.
-// Wrap all your components with this HOC to enable the use of Dependency Injection feature.
-// This should be used for all your pages to be able to utilize the Dependency Injection feature.
-
-
 import { ComponentProps, ElementType } from "react";
 import { container } from "../dependencies";
-
 
 export const WithDependency = (
     Component:ElementType,
